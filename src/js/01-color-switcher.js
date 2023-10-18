1799ecdf7cd3188fc922f8c2d937a05360cce17b
Rash AI <RashAI@dtek.com>
2023-10-18 19:13:16 +0300

    init3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const buttonStartRef = document.querySelector('[data-start]');
+const buttonStopRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
 
+buttonStartRef.addEventListener('click', onButtonStart);
+buttonStopRef.addEventListener('click', onButtonStop);
+
+let intervalId;
+function onButtonStart(event) {
+  buttonStartRef.disabled = true;
+  buttonStopRef.disabled = false;
+  intervalId = setInterval(() => {
+    bodyRef.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+}
+
+function onButtonStop(event) {
+  buttonStopRef.disabled = true;
+  buttonStartRef.disabled = false;
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
